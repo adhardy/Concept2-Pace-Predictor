dc04ae9c057b964bddaecbaa570be6c864b83938
Adam Hardy <adh167@gmail.com>
2021-03-27 22:18:50 +0000

    load data from new analysis csv's

---
+++ pacepredictor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class Predictor():
+
+    def __init__(self):
+        pass
+
+    def load_csvs(self, input_path:str):
+        """Load the input csv into a pandas dataframe (self.df)"""
+        self.df_athletes = pd.read_csv(f"{input_path}/athletes_clean.csv")
+        self.df_workouts = pd.read_csv(f"{input_path}/workouts_clean.csv")
+
