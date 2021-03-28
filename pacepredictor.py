5e5c0e740993c251a83b890f7031b7d189a71ade
Adam Hardy <adh167@gmail.com>
2021-03-28 18:42:45 +0100

    visualisation

--- pacepredictor.py
+++ pacepredictor.py
@@ -8,4 +8,8 @@ class Predictor():
     def load_csv(self, file:str):
         """Load the input csv into a pandas dataframe (self.df)"""
         self.df = pd.read_csv(f"{file}", index_col=0)
+        self.df_dummies = pd.get_dummies(self.df)
 
+    def split_data(self, y_col)
+        self.y = self.df_dummies[y_col].to_list()
+        self.X - self,df_dummies
