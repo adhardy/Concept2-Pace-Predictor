35ce6218f28dd60392f45bcaa74c457dfe2c9bbf
Adam Hardy <adh167@gmail.com>
2021-03-28 22:01:25 +0100

    first full LR model

--- pacepredictor.py
+++ pacepredictor.py
@@ -10,7 +10,8 @@ class Predictor():
         if self.dummies:
             self.df_no_dummies = self.df
             self.df = pd.get_dummies(self.df)
-        
+        #replace all space in column names with underscores for patsy
+        self.df = self.df.rename(columns=lambda x: x.replace(" ", "_"))
 
     def load_csv(self, file:str):
         """Load the input csv into a pandas dataframe (self.df)"""
@@ -19,24 +20,28 @@ class Predictor():
 
     def split_data(self):
         columns = self.df.columns
+        dtypes = self.df.dtypes.to_dict()
         np_arr = self.df.to_numpy()
 
         arr_train, arr_test = train_test_split(np_arr, test_size=0.3, random_state=self.random_state)
 
         self.df_train = pd.DataFrame(arr_train)
         self.df_train.columns = columns
+        self.df_train = self.df_train.astype(dtypes)
 
         arr_test, arr_val = train_test_split(arr_test, test_size=0.5, random_state=self.random_state)
 
         self.df_test = pd.DataFrame(arr_test)
         self.df_test.columns = columns
+        self.df_test = self.df_test.astype(dtypes)
 
         self.df_val = pd.DataFrame(arr_val)
         self.df_val.columns = columns
+        self.df_val = self.df_val.astype(dtypes)
 
 def create_lr_formula(y_col:str, x_cols:list):
 
-    formula = f"{y_col} ~"
+    formula = f"{y_col} ~ "
     for x_col in x_cols:
         formula += f"{x_col} + "
     
