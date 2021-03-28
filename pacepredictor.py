23791193c75c75c6dd1fcfbd57fc885c3fbddd68
Adam Hardy <adh167@gmail.com>
2021-03-28 18:50:18 +0100

    added split_data

--- pacepredictor.py
+++ pacepredictor.py
@@ -9,7 +9,7 @@ class Predictor():
         """Load the input csv into a pandas dataframe (self.df)"""
         self.df = pd.read_csv(f"{file}", index_col=0)
         self.df_dummies = pd.get_dummies(self.df)
-
-    def split_data(self, y_col)
+    
+    def split_data(self, y_col):
         self.y = self.df_dummies[y_col].to_list()
-        self.X - self,df_dummies
+        self.X = self.df_dummies.loc[:, self.df_dummies.columns != y_col]
