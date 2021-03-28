3db36785cd56bc2f2fe1fea26fee1a85ee75eec3
Adam Hardy <adh167@gmail.com>
2021-03-28 19:23:45 +0100

    split_data updated

--- pacepredictor.py
+++ pacepredictor.py
@@ -3,20 +3,33 @@ from sklearn.model_selection import train_test_split
 
 class Predictor():
 
-    def __init__(self, file, dummies=True):
+    def __init__(self, file, dummies=True, random_state=None):
         self.load_csv(file)
         self.dummies = dummies
+        self.random_state = random_state
         if self.dummies:
             self.df_no_dummies = self.df
             self.df = pd.get_dummies(self.df)
+        
 
     def load_csv(self, file:str):
         """Load the input csv into a pandas dataframe (self.df)"""
         self.df = pd.read_csv(f"{file}", index_col=0)
         self.df_dummies = pd.get_dummies(self.df)
 
-    def split_data(self, df):
-        # self.y = self.df_dummies[y_col].to_list()
-        # self.X = self.df_dummies.loc[:, self.df_dummies.columns != y_col]
-        columns = self.df_dummies.columns
+    def split_data(self):
+        columns = self.df.columns
+        np_arr = self.df.to_numpy()
 
+        arr_train, arr_test = train_test_split(np_arr, test_size=0.3, random_state=self.random_state)
+
+        self.df_train = pd.DataFrame(arr_train)
+        self.df_train.columns = columns
+
+        arr_test, arr_val = train_test_split(arr_test, test_size=0.5, random_state=self.random_state)
+
+        self.df_test = pd.DataFrame(arr_test)
+        self.df_test.columns = columns
+
+        self.df_val = pd.DataFrame(arr_val)
+        self.df_val.columns = columns
