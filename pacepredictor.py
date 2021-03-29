eaebabc5b41f1a9987e6c888c74f8b15be21834f
Adam Hardy <adh167@gmail.com>
2021-03-29 14:42:37 +0100

    added LinearRegressionIteration

--- pacepredictor.py
+++ pacepredictor.py
@@ -2,6 +2,76 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import plotly.graph_objects as go
 import statsmodels.formula.api as smf
+import numpy as np
+
+class LinearRegressionIteration():
+    def __init__(self, linear_regression):       
+        
+        self.df_train = linear_regression.df_train.copy() #make sure we have copies as the model will alter the contents
+        self.df_test = linear_regression.df_test.copy()
+        self.df_val = linear_regression.df_val.copy()
+
+        self.col_y = linear_regression.col_y
+        self.cols_x = linear_regression.cols_x
+
+        self.order = linear_regression.order
+        self.descrption = linear_regression.descrption
+        
+        self.formula = self.get_formula()
+
+    def get_formula(self):
+        formula = f"{self.col_y} ~ "
+        for col_x in self.cols_x:
+            formula += f"{col_x} + "
+        return formula[:-3]
+
+    def fit(self):
+        self.model = smf.ols(formula=self.formula, data=self.df_train).fit()
+        print(self.model.summary())
+
+    def predict(self):
+        self.col_pred = f"{self.col_y}_pred"
+        self.df_test[self.col_pred] = self.model.predict(self.df_test[self.cols_x])
+        self.df_test["squared_error"] = squared_error(self.df_test, self.col_y, self.col_pred)
+        self.df_test["error"] = np.sqrt(self.df_test["squared_error"])
+        self.mse = self.df_test["squared_error"].mean()
+        self.mean_error = self.df_test["error"].mean()
+
+    def stats(self):
+        print(f"Mean squared error: {round(self.mse,1)}")
+        print(f"Mean error: {round(self.mean_error,1)}")
+
+def squared_error(df, col_a, col_b):
+    return (df[col_a] - df[col_b])**2
+
+def create_linear_formula(col_y:str, cols_x:list):
+    formula = f"{col_y} ~ "
+    for col_x in cols_x:
+        formula += f"{col_x} + "
+    
+    return formula[:-3]
+
+class LinearRegression():
+
+    def __init__(self, df_train, df_test, df_val, col_y:str, cols_x:list, order:int, description:str=None, alpha:float=0.05):       
+        
+        self.df_train = df_train.copy() #make sure we have copies as the model will alter the contents
+        self.df_test = df_test.copy()
+        self.df_val = df_val.copy()
+
+        self.alpha = alpha
+
+        self.col_y = col_y
+        self.cols_x = cols_x
+
+        self.order = order
+        self.descrption = description
+
+        self.iterations = []
+
+    def iterate(self):
+        #if all p values are below alpha, stop
+        self.iterations.append(LinearRegressionIteration(self))
 
 class Predictor():
 
@@ -42,45 +112,8 @@ class Predictor():
         self.df_val.columns = columns
         self.df_val = self.df_val.astype(dtypes)
 
-    def add_model(self, model_name:str, ycol:str, xcols:list, order:int=1, descrption:str=None):
-        self.models[model_name] = self.LinearRegressionModel(self.df_train, self.df_test, self.df_val, ycol, xcols, order, descrption)
-
-    class LinearRegressionModel():
-
-        def __init__(self, df_train, df_test, df_val, ycol:str, xcols:list, order:int, descrption:str):       
-            
-            self.df_train = df_train
-            self.df_test = df_test
-            self.df_val = df_val
-
-            self.ycol = ycol
-            self.xcols = xcols
-
-            self.order = order
-            self.descrption = descrption
-            
-            self.formula = self.get_formula()
-
-        def get_formula(self):
-            formula = f"{self.ycol} ~ "
-            for xcol in self.xcols:
-                formula += f"{xcol} + "
-                return formula[:-3]
-
-        def fit(self):
-            self.model = smf.ols(formula=self.formula, data=self.df_train).fit()
-            print(self.model.summary())
-
-        def predict(self):
-            pass
-
-def create_linear_formula(y_col:str, x_cols:list):
-
-    formula = f"{y_col} ~ "
-    for x_col in x_cols:
-        formula += f"{x_col} + "
-    
-    return formula[:-3]
+    def add_model(self, model_name:str, col_y:str, cols_x:list, order:int=1, descrption:str=None, alpha:float=0.05):
+        self.models[model_name] = LinearRegression(self.df_train, self.df_test, self.df_val, col_y, cols_x, order, descrption, alpha)
 
 # Creating a general plotting function for plotting a scatter plot and line on the same figure
 def plot_scatter_and_line(x, scatter_y, line_y, scatter_name, line_name, title, x_title, y_title):
@@ -96,6 +129,6 @@ def plot_scatter_and_line(x, scatter_y, line_y, scatter_name, line_name, title, 
     
     return fig
 
-def get_xcols(df, ycol):
-    """get a list of all the columns names not matching ycol"""
-    return df.loc[:,df.columns != ycol].columns
+def get_cols_x(df, col_y):
+    """get a list of all the columns names not matching col_y"""
+    return df.loc[:,df.columns != col_y].columns
